b2d8a8bb5f129afe69775de87c96d3b718917ba6
Felipe Ribeiro <piporibeiro@gmail.com>
2022-11-24 18:50:38 -0300

    feat: class loops

---
+++ sintaxes-variables/src/Loops.java
@@ -0,0 +1,11 @@
+public class Loops {
+    public static void main(String[] args) {
+        for (int multiply = 1 ; multiply <= 10; multiply++){
+            for (int count = 0; count <= 10; count++){
+                System.out.print(multiply * count);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
